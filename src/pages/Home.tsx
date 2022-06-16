976228a8cd927bb34cb3bba419322b5e309f124a
hirm159 <hiro159m@gmail.com>
2022-06-16 23:35:36 +0900

    leaflet表示

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import "leaflet/dist/leaflet.css"
 
 import { FrontVar } from '../components/FrontBar';
 import { Container, Row } from 'react-bootstrap';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { LatLng } from "leaflet";
 
 export default class Home extends React.Component {
   render() {
+    const position = new LatLng(51.505, -0.09);
+
     return (
         <Container>
           <Row>
             <FrontVar />
           </Row>
           <Row>
+          <MapContainer center={position} zoom={13} style={{height: 400}}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </MapContainer>
           </Row>
       </Container>
     )
