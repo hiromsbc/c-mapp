8e12e5be9ec2943fb8829ca942368037776e3900
hirm159 <hiro159m@gmail.com>
2022-08-07 13:07:47 +0900

    リンク集の追加

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FrontVar } from '../components/FrontBar';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Table } from 'react-bootstrap';
 
 export default class Links extends React.Component {
   render() {
@@ -11,7 +11,22 @@ export default class Links extends React.Component {
           <FrontVar />
         </Row>
         <Row>
-          
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>remarks</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><a href="https://hirm159.hatenablog.jp/" target="_blank" rel="noopener noreferrer">Auther</a></td>
+                <td>作者HP</td>
+              </tr>
+            </tbody>
+          </Table>
         </Row>
       </Container>
     )
